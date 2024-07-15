2deff0ff91e08f9a57874b0f2b82d600c021bde7
Vane23Padilla <117601217+Vane23Padilla@users.noreply.github.com>
2024-07-15 15:26:57 -0500

    Footer v0.2

--- cartera-vencida/src/App.tsx
+++ cartera-vencida/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Bar_Header from './components/bar_Header'; // ajusta la ruta según tu estructura de archivos
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,7 +11,9 @@ function App() {
         <h1>Bienvenido a EP-EMA</h1>
         {/* Otros componentes y contenido de tu aplicación */}
       </main>
+      <Footer/>
     </div>
+    
   );
 }
 
--- cartera-vencida/src/components/footer.tsx
+++ cartera-vencida/src/components/footer.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './Footer.css'; // Asegúrate de que el archivo CSS esté en la misma carpeta
+import logoEpema from '../resources/images/logoEpema-Photoroom.png';
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div className="footer-links">
-        <h4>Links de Interés</h4>
-        <img 
-        src="https://ambato-ema.gob.ec/wp-content/uploads/2023/03/epema-1.jpg" 
-        alt="Icono EP-EMA" 
-        className="footer-icon" 
-      />
+        <img
+          alt="EP-EMA Logo"
+          src={logoEpema}
+          width={200}
+          height={200}
+        />
       </div>
       <div className="footer-center">
         <h4>El centro de acopio más grande el Ecuador</h4>
@@ -19,9 +20,7 @@ const Footer: React.FC = () => {
         <p>EP-EMA © {new Date().getFullYear()}. Todos los derechos reservados.</p>
       </div>
       <div className="footer-social">
-        <img src="URL_ICONO_FACEBOOK" alt="Facebook" />
-        <img src="URL_ICONO_TWITTER" alt="Twitter" />
-        <img src="URL_ICONO_INSTAGRAM" alt="Instagram" />
+        {/* Aquí podrías agregar iconos sociales si es necesario */}
       </div>
     </footer>
   );
