2eb34fd1b25de5334a21aee2f6ec93a21658e2b0
Vane23Padilla <117601217+Vane23Padilla@users.noreply.github.com>
2024-07-15 16:04:49 -0500

    Body :v

--- cartera-vencida/src/App.tsx
+++ cartera-vencida/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Bar_Header from './components/bar_Header'; // ajusta la ruta según tu estructura de archivos
 import Footer from './components/Footer';
+import DataTable from './components/Table';
 
 function App() {
   return (
@@ -9,6 +10,10 @@ function App() {
       {/* Aquí puedes agregar el resto de tu contenido */}
       <main>
         <h1>Bienvenido a EP-EMA</h1>
+        <div style={}>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum recusandae iste error minima illo odio id enim, atque aliquam ratione voluptatem, similique perspiciatis aut saepe tenetur accusamus in rerum labore.</div>
+    <div><DataTable/></div>
+  </div> 
         {/* Otros componentes y contenido de tu aplicación */}
       </main>
       <Footer/>
