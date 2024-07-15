ff976967ad0fcc382b56b6380112ca1748f5288a
Gabriel Alejandro Ocampo <86894475+GAOT539@users.noreply.github.com>
2024-07-15 15:07:12 -0500

    app

--- cartera-vencida/src/App.tsx
+++ cartera-vencida/src/App.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import './App.css';
-<<<<<<< HEAD
-import Bar_Header from './components/bar_Header';
-=======
-import Dashboard from './components/Dashboard_01';
-import Footer from './components/Footer'; // Asegúrate de que la ruta sea correcta
->>>>>>> a7aed8fad3a87948130531319c7c38a409868fa9
+import Bar_Header from './components/bar_Header'; // ajusta la ruta según tu estructura de archivos
 
 function App() {
   return (
-    <div className="App">
-<<<<<<< HEAD
-      <Bar_Header/>
-=======
-      <Dashboard />
-      <Footer />
->>>>>>> a7aed8fad3a87948130531319c7c38a409868fa9
+    <div>
+      <Bar_Header />
+      {/* Aquí puedes agregar el resto de tu contenido */}
+      <main>
+        <h1>Bienvenido a EP-EMA</h1>
+        {/* Otros componentes y contenido de tu aplicación */}
+      </main>
     </div>
   );
 }
