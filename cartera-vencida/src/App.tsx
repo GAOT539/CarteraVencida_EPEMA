0fc02f906ed194a573a6c498b630cbe084545dcf
Sebastian Tixilema <sebastixi2002@gmail.com>
2024-07-18 14:37:35 -0500

    Router Dom

--- cartera-vencida/src/App.tsx
+++ cartera-vencida/src/App.tsx
@@ -1,21 +1,27 @@
+import Bar_Header from './components/bar_Header';
 import Bar_Footer from './components/bar_Footer';
 import Bodega from './components/body_Historical';
-import Bar_Header from './components/bar_Header';
 import Body_Information from './components/body_Information';
-import GeneradorPDF from './components/generator_PDF_v0.1';
-import GeneradorPDFv02 from './components/generator_PDF_v0.2';
-import User from './components/user';
+import GeneradorPDF from './components/generator_PDF';
+import Body_User from './components/login';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Bar_Header />
-      <main>
-        <User/>
-      </main>
-      <Bar_Footer />
-    </div>
-    
+    <BrowserRouter>
+      <div>
+        <Bar_Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Bodega />} />
+            <Route path="/information" element={<Body_Information />} />
+            <Route path="/login" element={<Body_User />} />
+          </Routes>
+        </main>
+        <Bar_Footer />
+      </div>
+    </BrowserRouter>
   );
 }
+
 export default App;
