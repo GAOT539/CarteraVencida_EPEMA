ef8f8758b9a1e22fac4e241bddd8219f5361bf88
Gabriel Alejandro Ocampo <86894475+GAOT539@users.noreply.github.com>
2024-07-19 15:44:55 -0500

    GeneradorPDF Antepenultima

--- cartera-vencida/src/resources/style/style_Generator_PDF.ts
+++ cartera-vencida/src/resources/style/style_Generator_PDF.ts
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
   },
   title: {
     fontFamily: "Roboto",
-    fontSize: 18,
+    fontSize: 16,
     textAlign: 'center',
     flex: 1,
     fontWeight: "bold"
@@ -57,30 +57,110 @@ const styles = StyleSheet.create({
   reportNumber_N: {
     color: colors.text_Red
   },
-  section01: {
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    paddingHorizontal: 30,
-    marginBottom: 10
-  },
-  section02: {
-    alignItems: 'center',
-    paddingHorizontal: 30,
-    marginBottom: 10
-  },
   texts: {
     fontFamily: "Roboto",
     fontSize: 10,
     flexGrow: 1,
-    textAlign: 'justify'
+    textAlign: 'justify',
   },
-  texts_Black: {
+  texts_Blod: {    
+    fontSize: 10,
     fontFamily: "Roboto",
     fontWeight: "bold",
-    fontSize: 10,
-    flex: 1
+    color: colors.text_Red,
+    textTransform: "uppercase"
   },
+  texts_Italic:{
+    fontFamily: "Roboto",
+    fontStyle: 'italic',
+    fontSize: 10,
+    textAlign: 'justify',
+  },
+  section01: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginBottom: 5
+  },
+  section02: {
+    alignItems: 'center',
+    marginBottom: 5
+  },
+  section03: {
+    alignItems: 'flex-start',
+    marginBottom: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  section04: {
+    alignItems: 'center',
+    marginBottom: 1
+  },
+  section05: {
+    alignItems: 'center',
+    marginBottom: 1
+  }, 
+  section06: {
+    alignItems: 'center',
+    marginBottom: 1
+  }, 
+  roundedSquare: {
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: colors.text_Black,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    marginVertical: 10,
+  },
+  table: {
+    width: 275,
+    borderWidth: 0.4,
+    borderColor: colors.text_Black,
+    marginBottom: 5,
+  },
+  tableRow: {
+    flexDirection: 'row',
+  },
+  tableCol: {
+    width: '50%',
+    borderWidth: 0.4,
+    borderColor: colors.text_Black,
+    padding: 5,
+  },
+  tableCell: {
+    fontSize: 10,
+  },
+  notificationTextContainer: {
+    marginLeft: 10,
+    flex: 1,
+  },
+  notificationText: {
+    fontSize: 10,
+    marginBottom: 5,
+  },
+
+  table2: {
+    width: '100%',
+    borderWidth: 0.5,
+    borderColor: '#000',
+    marginBottom: 5,
+  },
+  table2Row: {
+    flexDirection: 'row',
+  },
+  table2Col: {
+    width: '14.28%',
+    borderWidth: 0.5,
+    borderColor: '#000',
+    padding: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  table2Cell: {
+    fontSize: 8,
+    textAlign: 'center',
+  }
 });
 
 export default styles;
