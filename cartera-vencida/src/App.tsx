28e869d1026dbf2833881a590893daa37d997caf
Vane23Padilla <117601217+Vane23Padilla@users.noreply.github.com>
2024-07-19 09:42:25 -0500

    App casi final

--- cartera-vencida/src/App.tsx
+++ cartera-vencida/src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Bar_Header from './components/bar_Header';
 import Bar_Footer from './components/bar_Footer';
 import Body_Historical from './components/body_Historical';
@@ -9,22 +9,35 @@ import Body_User from './components/body_User';
 import Body_Error from './components/body_Error';
 import GeneradorPDF from './components/generator_PDF';
 
+const AppRoutes: React.FC = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        // Limpiar estado de autenticación cuando la ruta cambia
+        localStorage.removeItem('isAuthenticated');
+    }, [location]);
+
+    return (
+        <Routes>
+            <Route path="/" element={<Body_Information />} />
+            <Route path="/historicos" element={<Body_Historical />} />
+            <Route path="/login" element={<Body_Login />} />
+            <Route path="/pdf" element={<GeneradorPDF />} />
+            <Route element={<Body_Error isProtected />}>
+                <Route path="/usuarios" element={<Body_User />} />
+            </Route>
+            <Route path="*" element={<Body_Error />} />
+        </Routes>
+    );
+};
+
 function App() {
     return (
         <BrowserRouter>
             <div>
                 <Bar_Header />
                 <main>
-                    <Routes>
-                        <Route path="/" element={<Body_Information />} />
-                        <Route path="/historicos" element={<Body_Historical />} />
-                        <Route path="/login" element={<Body_Login />} />
-                        <Route path="/pdf" element={<GeneradorPDF />} />
-                        <Route element={<Body_Error isProtected />}>
-                            <Route path="/usuarios" element={<Body_User />} />
-                        </Route>
-                        <Route path="*" element={<Body_Error />} />
-                    </Routes>
+                    <AppRoutes />
                 </main>
                 <Bar_Footer />
             </div>
