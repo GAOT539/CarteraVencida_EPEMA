2d80ec0e3cfe7efdded1c72b97f9190de4c552b0
Gabriel Alejandro Ocampo <gabrielocampot@gmail.com>
2024-08-19 09:19:46 -0500

    AutoInicio

---
+++ AutoInicio01.bat
@@ -0,0 +1,6 @@
+@echo off
+cd C:\ruta\a\tu\proyecto\backend
+start npm run start
+cd C:\ruta\a\tu\proyecto\frontend
+start npm run dev
+exit
---
+++ AutoInicio02.vbs
@@ -0,0 +1,3 @@
+Set WshShell = CreateObject("WScript.Shell") 
+WshShell.Run "C:\ruta\a\tu\archivo.bat", 0
+Set WshShell = Nothing
